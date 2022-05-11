2526c154ee5573599786a99b93a203de29c7f653
afwan <mnsmackys@gmail.com>
2022-05-11 11:59:43 +0600

    Assinment 09 uploaded

---
+++ app9.js
@@ -0,0 +1,70 @@
+
+// const devs = [
+//     {
+//         id : 1,
+//         name : "Asraf",
+//         age : 25,
+//         skill : "IOS Devs"
+//     },
+//     {
+//         id : 2,
+//         name : "Mhabub",
+//         age : 20,
+//         skill : "BlockChain Devs"
+//     },
+//     {
+//         id : 3,
+//         name : "Anisul",
+//         age : 30,
+//         skill : "PHP Devs"
+//     },
+//     {
+//         id : 4,
+//         name : "Subir",
+//         age : 40,
+//         skill : "MERN Devs"
+//     }
+// ]
+
+// localStorage.setItem('devs', JSON.stringify(devs));
+
+
+// let data = `[{"id":1,"name":"Asraf","age":25,"skill":"ISO Devs"},
+// {"id":2,"name":"Mhabub","age":20,"skill":"BlockChain Devs"},
+// {"id":3,"name":"Anisul","age":30,"skill":"PHP Devs"},
+// {"id":4,"name":"Subir","age":40,"skill":"MERN Devs"}]`;
+
+// console.log(JSON.parse(data));
+// localStorage.setItem('name','jamal');
+// localStorage.setItem('age',200);
+// localStorage.setItem('skill','ios dev');
+
+// sessionStorage.setItem('food','alo');
+// sessionStorage.setItem('game','ha do do')
+
+// console.log(localStorage.getItem('name'));
+// console.log(localStorage.getItem('age'));
+// console.log(localStorage.getItem('skill'));
+
+// console.log(sessionStorage.getItem('food'))
+
+// localStorage.removeItem('name');
+// localStorage.removeItem('skill');
+// localStorage.removeItem('age');
+
+// let data = JSON.parse(localStorage.getItem('devs'));
+
+// for(let x in data){
+//     console.log(data[x]);
+// }
+
+
+// document.cookie = "name=Karim";
+// document.cookie = "age=30";
+// document.cookie = "skill=ios devs";
+
+// console.log(document.cookie);
+
+// document.cookie = "name=''"
+// document.cookie = "age=''"
+// document.cookie = "skill=''"
